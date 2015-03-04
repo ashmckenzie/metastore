7fe1f6ae0d6427c45536cb0ddb71302d0b5a4e3c
Ash McKenzie <ash@the-rebellion.net>
2015-03-04 16:21:14 +1100

    Load rspec, pry-byebug, CodeClimate coverage and SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,12 @@
+require 'pry-byebug'
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
+require 'simplecov'
+SimpleCov.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+Dir["./spec/support/**/*.rb"].sort.each { |f| require f}
+
 require 'metastore'
