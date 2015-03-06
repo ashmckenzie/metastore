5096f55d7ac170a1be7a9a302315aa8f48d3cae5
Ash McKenzie <ash@the-rebellion.net>
2015-03-06 12:44:49 +1100

    When using pry-byebug with fakefs, loading/saving history needs to be disabled

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 require 'fakefs/safe'
 require 'fakefs/spec_helpers'
+
 require 'pry-byebug'
+Pry.config.history.should_load = false
+Pry.config.history.should_save = false
+
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 
